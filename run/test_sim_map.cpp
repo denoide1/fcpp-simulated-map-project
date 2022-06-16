6bb641d8daded3a398f9627d01b988090721144e
Gianmarco Rampulla <45428938+denoide1@users.noreply.github.com>
2022-06-16 23:06:57 +0200

    update for city test

--- run/test_sim_map.cpp
+++ run/test_sim_map.cpp
@@ -8,7 +8,6 @@
 // [INTRODUCTION]
 //! Importing the FCPP library.
 #include "lib/fcpp.hpp"
-
 /**
  * @brief Namespace containing all the objects in the FCPP library.
  */
@@ -22,7 +21,11 @@ bool operator<(vec<n> const& a, vec<n> const& b) {
     return true;
 }
 
-    //! @brief Dimensionality of the space.
+std::ostream& operator<<(std::ostream& o, map_navigator const&) {
+    return o;
+}
+
+//! @brief Dimensionality of the space.
 constexpr size_t dim = 2;
 //! @brief Side of the deployment area.
 constexpr size_t width = 500;
@@ -52,7 +55,20 @@ namespace tags {
     struct obstacle_delta_y {};
     //! @brief Distance from closest neighbour
     struct distance_min_nbr {};
+    
+    struct current_room {};
+
+    struct dest_room {};
+    
+    struct current_waypoint {};
+
+    struct current_room_waypoint {};
+
+    struct dest_room_waypoint {};
+
+    struct distances_room_waypoints {};
 }
+
 
 
 //! @brief Main function.
@@ -61,7 +77,7 @@ MAIN() {
     node.storage(tags::node_size{}) = 10;
     node.storage(tags::node_color{}) = color(GREEN);
     node.storage(tags::node_shape{}) = shape::sphere;
-
+    
     // used to set position of out of bound nodes at the start
     if (coordination::counter(CALL) == 1) {
         if (node.net.is_obstacle(node.position())) {
@@ -73,37 +89,31 @@ MAIN() {
         }
     }
 
-    auto closest = node.net.closest_obstacle(node.position());
-    real_t dist1 = distance(closest, node.position());
-    real_t min_neighbor_dist = min_hood(CALL, node.nbr_dist(),std::numeric_limits<real_t>::max());
+    /*
+	node.storage(tags::current_room{}) = node.net.get_current_room(node.position());
+    node.storage(tags::dest_room{}) = node.net.get_current_room(make_vec(300, 300));
+    node.storage(tags::current_room_waypoint{}) = node.net.get_waypoints_room(node.position());
+    node.storage(tags::dest_room_waypoint{}) = node.net.get_waypoints_room(make_vec(300, 300));
+    node.storage(tags::distances_room_waypoints{}) = node.net.get_distance_from(node.position(),make_vec(300, 300));
+     */
 
-    node.storage(tags::nearest_obstacle{}) = closest;
-    node.storage(tags::distance_from_obstacle{}) = dist1;
-    node.storage(tags::distance_min_nbr{}) = min_neighbor_dist;
+    vec<2> next_step = node.net.path_to(node.position(),make_vec(300, 300));
 
-    if (dist1 <= 30) {
-        node.velocity() = make_vec(0,0);
-        node.propulsion() = make_vec(0,0);
-        node.propulsion() += -coordination::point_elastic_force(CALL,closest,1,0.10);
-        if (min_neighbor_dist <= 25) {
-            node.velocity() = make_vec(0,0);
-            node.propulsion() += -coordination::neighbour_elastic_force(CALL, 0.05, 0.05);
-        }
+
+    /*
+    if(std::abs(node.position()[0] - next_step[0]) > 0) {
+        next_step[0] += 1;
     }
-    else {
-        if (min_neighbor_dist <= 25) {
-            node.propulsion() = make_vec(0,0);
-            node.velocity() = make_vec(0,0);
-            node.propulsion() += -coordination::neighbour_elastic_force(CALL, 0.05, 0.05);
-        }
-        else {
-            node.propulsion() = make_vec(0,0);
-            rectangle_walk(CALL, make_vec(0, 0), make_vec(width, height), node.storage(tags::speed{}), 1);
-        }
-    }
+        next_step[1] += 1;
+    next_step[1] += 1;
+     */
 
 
 
+    node.storage(tags::current_waypoint{}) = next_step;
+    follow_target(CALL,next_step,20,1);
+    
+    
 }
 //! @brief Export types used by the main function (update it when expanding the program).
 FUN_EXPORT main_t = common::export_list<double, int, rectangle_walk_t<2>>;
@@ -121,7 +131,7 @@ using namespace component::tags;
 using namespace coordination::tags;
 
 //! @brief Number of people in the area.
-constexpr int node_num = 10;
+constexpr int node_num = 5;
 //! @brief Dimensionality of the space.
 constexpr size_t dim = 2;
 
@@ -135,7 +145,7 @@ using log_s = sequence::periodic_n<1, 0, 1>;
 //! @brief The sequence of node generation events (node_num devices all generated at time 0).
 using spawn_s = sequence::multiple_n<node_num, 0>;
 //! @brief The distribution of initial node positions (random in a 500x500 square).
-using rectangle_d = distribution::rect_n<1, 0, 0, 850, 500>;
+using rectangle_d = distribution::rect_n<1, 0, 0, 735, 418>;
 //! @brief The distribution of node speeds (all equal to a fixed value).
 using speed_d = distribution::constant_i<double, speed>;
 //! @brief The contents of the node storage as tags and associated types.
@@ -148,7 +158,13 @@ using store_t = tuple_store<
     speed,                      double,
     node_color,                 color,
     node_size,                  double,
-    node_shape,                 shape
+    node_shape,                 shape,
+    current_room,				std::vector<int>,
+    dest_room,                  std::vector<int>,
+    current_waypoint,			vec<2>,
+    current_room_waypoint,      std::vector<std::array<size_t,2>>,
+    dest_room_waypoint,         std::vector<std::array<size_t,2>>,
+    distances_room_waypoints,  std::vector<real_t>
 >;
 //! @brief The tags and corresponding aggregators to be logged (change as needed).
 using aggregator_t = aggregators<
@@ -176,7 +192,7 @@ DECLARE_OPTIONS(list,
     shape_tag<node_shape>, // the shape of a node is read from this tag in the store
     size_tag<node_size>,   // the size  of a node is read from this tag in the store
     color_tag<node_color>,  // the color of a node is read from this tag in the store
-    area<0,0,850,500,1>
+    area<0,0,1200,800,1>
 );
 
 } // namespace option
@@ -188,8 +204,10 @@ int main() {
     using namespace fcpp;
     //! @brief The network object type (interactive simulator with given options).
     using net_t = component::interactive_simulator<option::list>::net;
-    //! @brief The initialisation values (simulation name).
-    auto init_v = common::make_tagged_tuple<option::name, option::texture, option::obstacles, option::speed, option::obstacles_color_threshold>("Simulated map test", "building2.jpeg", "building2.jpeg", 3, 0.8);
+
+    map_navigator obj = map_navigator("oslo2.png");
+     //! @brief The initialisation values (simulation name).
+    auto init_v = common::make_tagged_tuple<option::name, option::texture, fcpp::component::tags::map_navigator_obj, option::speed>("Simulated map test", "./debugPointsOslo.png", obj, 1.5);
     //! @brief Construct the network object.
     net_t network{init_v};
     //! @brief Run the simulation until exit.
